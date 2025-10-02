2d810eed7ec39465e0941b3394fb64081b118c77
Kyle <1kylekim1@gmail.com>
2025-10-02 16:35:18 -0400

    Added scoreboard, buttons, and round results

--- script.js
+++ script.js
@@ -14,51 +14,100 @@ function getComputerChoice() {
     }
 }
 
-function playGame() {
-    let humanScore = 0;
-    let computerScore = 0;
-    
-    function playRound(humanChoice, computerChoice) {
+let humanScore = 0;
+let computerScore = 0;
+
+const scoreboardDiv = document.querySelector("#scoreboard");
+
+const humanScoreboard = document.createElement("p");
+humanScoreboard.setAttribute("id", "humanScoreboard");
+scoreboardDiv.appendChild(humanScoreboard).textContent = "Human: " + humanScore;
+
+const computerScoreboard = document.createElement("p");
+computerScoreboard.setAttribute("id", "computerScoreboard");
+scoreboardDiv.appendChild(computerScoreboard).textContent = "Computer: " + computerScore;
+
+function handleRockClick() {
+    playRound("rock", getComputerChoice());
+}
+
+function handlePaperClick() {
+    playRound("paper", getComputerChoice());
+}
+
+function handleScissorsClick() {
+    playRound("scissors", getComputerChoice());
+}
+
+const playerOptionsDiv = document.querySelector("#player-options");
+
+const rockBtn = document.createElement("button");
+rockBtn.setAttribute("id", "rock-btn");
+rockBtn.innerText = "Rock";
+rockBtn.addEventListener("click", handleRockClick);
+playerOptionsDiv.appendChild(rockBtn);
+
+const paperBtn = document.createElement("button");
+paperBtn.setAttribute("id", "paper-btn");
+paperBtn.innerText = "Paper";
+paperBtn.addEventListener("click", handlePaperClick);
+playerOptionsDiv.appendChild(paperBtn);
+
+const scissorsBtn = document.createElement("button");
+scissorsBtn.setAttribute("id", "scissors-btn");
+scissorsBtn.innerText = "Scissors";
+scissorsBtn.addEventListener("click", handleScissorsClick);
+playerOptionsDiv.appendChild(scissorsBtn);
+
+const resultsDiv = document.querySelector("#results");
+
+function playRound(humanChoice, computerChoice) {
+        const roundResult = document.createElement("p");
         if (computerChoice == "rock") {
             if (humanChoice == "rock") {
-                console.log("Tie!");
+                resultsDiv.appendChild(roundResult).textContent = "Tie!";
             } else if (humanChoice == "paper") {
-                console.log("You win! Paper beats Rock.");
+                resultsDiv.appendChild(roundResult).textContent = "You win! Paper beats Rock.";
                 humanScore++;
             } else if (humanChoice == "scissors") {
-                console.log("You lose! Rock beats Paper.");
+                resultsDiv.appendChild(roundResult).textContent = "You lose! Rock beats Scissors.";
                 computerScore++;
             }
         } else if (computerChoice == "paper") {
             if (humanChoice == "rock") {
-                console.log("You lose! Paper beats Rock.");
+                resultsDiv.appendChild(roundResult).textContent = "You lose! Paper beats Rock.";
                 computerScore++;
             } else if (humanChoice == "paper") {
-                console.log("Tie!");
+                resultsDiv.appendChild(roundResult).textContent = "Tie!";
             } else if (humanChoice == "scissors") {
-                console.log("You win! Scissors beats Paper.");
+                resultsDiv.appendChild(roundResult).textContent = "You win! Scissors beats Paper.";
                 humanScore++;
             }
         } else if (computerChoice == "scissors") {
             if (humanChoice == "rock") {
-                console.log("You win! Rock beats Scissors.");
+                resultsDiv.appendChild(roundResult).textContent = "You win! Rock beats Scissors.";
                 humanScore++;
             } else if (humanChoice == "paper") {
-                console.log("You lose! Scissors beats Paper.");
+                resultsDiv.appendChild(roundResult).textContent = "You lose! Scissors beats Paper.";
                 computerScore++;
             } else if (humanChoice == "scissors") {
-                console.log("Tie!");
+                resultsDiv.appendChild(roundResult).textContent = "Tie!";
             }
-        }   
-    }
+        }
 
-    for (let i = 0; i < 5; i++) {
-        const humanSelection = getHumanChoice();
-        const computerSelection = getComputerChoice();
-        playRound(computerSelection, humanSelection);
-    }
+        document.querySelector("#humanScoreboard").innerText = "Human: " + humanScore;
+        document.querySelector("#computerScoreboard").innerText = "Computer: " + computerScore;
 
-    console.log("Game over!" + "\n" + "Human: " + humanScore + "\n" + "Computer: " + computerScore + "\n");
+        if (humanScore == 5 || computerScore == 5) {
+            rockBtn.removeEventListener("click", handleRockClick);
+            paperBtn.removeEventListener("click", handlePaperClick);
+            scissorsBtn.removeEventListener("click", handleScissorsClick);
+        if (humanScore == 5) {
+            const gameResult = document.createElement("p");
+            resultsDiv.appendChild(gameResult).innerText = "Game over! You win!";
+        } else {
+            const gameResult = document.createElement("p");
+            resultsDiv.appendChild(gameResult).innerText = "Game over! You lose!";
+        }
+    }
 }
-
-playGame();
